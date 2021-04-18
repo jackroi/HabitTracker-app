89b5cc9bfde3a449bacd1de37a4f81afc197cab0
Giacomo Rosin <giacomorosin@gmail.com>
2021-04-18 23:59:29 +0200

    Implement SectionList in home

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import { Button, StyleSheet, Text, View, FlatList, StatusBar, useColorScheme, TouchableOpacity } from 'react-native';
+import { Button, StyleSheet, Text, View, FlatList, StatusBar, useColorScheme, TouchableOpacity, SectionList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { MaterialIcons } from '@expo/vector-icons';
@@ -18,7 +18,8 @@ interface Habit {
   habitState: HabitState;
 }
 
-const FAKE_DATA: Habit[] = [
+
+const FAKE_DATA_1: Habit[] = [
   {
     id: "0",
     habitName: "Run",
@@ -41,6 +42,40 @@ const FAKE_DATA: Habit[] = [
   },
 ];
 
+const FAKE_DATA_2: Habit[] = [
+  {
+    id: "4",
+    habitName: "Hide",
+    habitState: HabitState.NOT_COMPLETED,
+  },
+  {
+    id: "5",
+    habitName: "Search",
+    habitState: HabitState.NOT_COMPLETED,
+  },
+  {
+    id: "6",
+    habitName: "Drive",
+    habitState: HabitState.NOT_COMPLETED,
+  },
+  {
+    id: "7",
+    habitName: "Go",
+    habitState: HabitState.NOT_COMPLETED,
+  },
+];
+
+const DATA = [
+  {
+    title: 'Productivity',
+    data: FAKE_DATA_1,
+  },
+  {
+    title: 'Sport',
+    data: FAKE_DATA_2,
+  },
+];
+
 
 const HomeScreen = ({ navigation }: HomeScreenNavigationProps) => {
   const theme = getTheme(useColorScheme());
@@ -57,11 +92,19 @@ const HomeScreen = ({ navigation }: HomeScreenNavigationProps) => {
     );
   };
 
+  const renderSectionHeader = ({ section: { title }}: { section: { title: string } }) => {
+    return (
+      <Text style={dynamicStyles.sectionHeader}>{title}</Text>
+    );
+  };
+
   return (
     <View style={dynamicStyles.container}>
-      <FlatList
-        data={FAKE_DATA}
+      <SectionList
+        sections={DATA}
         renderItem={renderItem}
+        renderSectionHeader={renderSectionHeader}
+        stickySectionHeadersEnabled={false}
       />
     </View>
   );
@@ -106,11 +149,19 @@ const HomeStackScreen = () => {
 const styles = (theme: Theme) => StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 20,
     backgroundColor: theme.colorBackground,
+  },
+  sectionHeader: {
+    backgroundColor: theme.colorBackground,
+    color: theme.colorOnBackground,
+    fontSize: 20,
+    paddingVertical: 10,
+    marginLeft: 20,
+    fontWeight: 'bold'
   },
   item: {
     marginVertical: 8,
+    paddingHorizontal: 20,
   },
   addButton: {
     flex: 1,
