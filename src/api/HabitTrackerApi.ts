f59b4638f3563c092780bd5b312b6335271cd034
Giacomo Rosin <giacomorosin@gmail.com>
2021-08-07 16:25:55 +0200

    Implement HabitTrackerApi.ts and install axios

--- src/api/HabitTrackerApi.ts
+++ src/api/HabitTrackerApi.ts
@@ -6,6 +6,9 @@
 
 import { DateTime } from "luxon";
 import axios, { AxiosInstance, AxiosError } from 'axios';
+import { AddHabitResponseBody, ErrorResponseBody, GetCategoriesResponseBody, GetGeneralStatsResponseBody, GetHabitHistoryResponseBody, GetHabitResponseBody, GetHabitsResponseBody, GetHabitStatsResponseBody, GetUserResponseBody, LoginResponseBody, RegistrationResponseBody, SuccessResponseBody } from "./httpTypes/responses";
+import { HabitType } from "./models/Habit";
+import { HistoryEntryType } from "./models/HistoryEntry";
 
 // type HabitHistory = HabitHistoryEntry[];
 
@@ -27,6 +30,7 @@ class HabitTrackerApi {
   private http: AxiosInstance;
   private token: string | null;
 
+  // TODO probabilmente singleton
   constructor() {
     this.http = axios.create({
       baseURL: HabitTrackerApi.BASE_URL,
@@ -52,22 +56,20 @@ class HabitTrackerApi {
     }
   }
 
+
+  // Authentication
+
+
+  /**
+   *
+   * @param name
+   * @param email
+   * @param password
+   * @returns
+   */
   async register(name: string, email: string, password: string): Promise<Result<string,string>> {
-    // TODO type
-    type RegistrationResponse = {
-      error: boolean,
-      statusCode: string,
-      token: string,
-    }
-
-    type ErrorResponse = {
-      error: boolean,
-      statusCode: string,
-      errorMessage: string,
-    }
-
     try {
-      const response = await this.http.request<RegistrationResponse>({
+      const response = await this.http.request<RegistrationResponseBody>({
         method: 'POST',
         url: '/register',
         data: {
@@ -79,47 +81,437 @@ class HabitTrackerApi {
       return ok(response.data.token);
     }
     catch (error) {
-      let errorMessage: string;
-      if (axios.isAxiosError(error)) {
-        // It's an AxiosError
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.warn(error.response.data);
-          console.warn(error.response.status);
-          console.warn(error.response.headers);
-          const errorBody = error.response.data as ErrorResponse;
-          errorMessage = errorBody.errorMessage
-        }
-        else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
-          console.warn(error.request);
-          errorMessage = 'Timeout error';
-          // TODO valutare un throw (in generale per gli errori di timeout)
-        }
-        else {
-          // Something happened in setting up the request that triggered an Error
-          console.error('Error', error.message);
-          errorMessage = 'Unknown error';
-        }
-        console.warn(error.config);
-      }
-      else {
-        // It's another type of error
-        errorMessage = 'Unknown error';
-        console.error(error);
-      }
+      const errorMessage: string = this.handleError(error);
       return err(errorMessage);
     }
   }
+
+
+  /**
+   *
+   * @param email
+   * @param password
+   * @returns
+   */
+  async login(email: string, password: string): Promise<Result<string,string>> {
+    try {
+      const response = await this.http.request<LoginResponseBody>({
+        method: 'GET',
+        url: '/login',
+        auth: {                     // Http Basic Authentication
+          username: email,
+          password: password,
+        },
+      });
+      return ok(response.data.token);
+    }
+    catch (error) {
+      const errorMessage: string = this.handleError(error);
+      return err(errorMessage);
+    }
+  }
+
+
+
+  // Account management
+
+  async getAccountInfo(): Promise<Result<GetUserResponseBody['user'],string>> {
+    this.ensureToken();
+
+    try {
+      const response = await this.http.request<GetUserResponseBody>({
+        method: 'GET',
+        url: '/user',
+        headers: {
+          'Authorization': `Bearer ${this.token}`,
+        },
+      });
+      return ok(response.data.user);
+    }
+    catch (error) {
+      const errorMessage: string = this.handleError(error);
+      return err(errorMessage);
+    }
+  }
+
+  async deleteAccount(): Promise<Result<null,string>> {
+    this.ensureToken();
+
+    try {
+      const response = await this.http.request<SuccessResponseBody>({
+        method: 'DELETE',
+        url: '/user',
+        headers: {
+          'Authorization': `Bearer ${this.token}`,
+        },
+      });
+      return ok(null);
+    }
+    catch (error) {
+      const errorMessage: string = this.handleError(error);
+      return err(errorMessage);
+    }
+  }
+
+
+  // Habit
+
+  async getHabits(
+    skip?: number,
+    limit?: number,
+    category?: string,
+    filter?: 'all' | 'archived' | 'active'
+  ): Promise<Result<GetHabitsResponseBody['habits'],string>> {
+    this.ensureToken();
+
+    try {
+      const response = await this.http.request<GetHabitsResponseBody>({
+        method: 'GET',
+        url: '/habits',
+        headers: {
+          'Authorization': `Bearer ${this.token}`,
+        },
+        params: {
+          skip: skip,
+          limit: limit,
+          category: category,
+          filter: filter,
+        },
+      });
+      return ok(response.data.habits);
+    }
+    catch (error) {
+      const errorMessage: string = this.handleError(error);
+      return err(errorMessage);
+    }
+  }
+
+  async createHabit(name: string, category: string, type: HabitType): Promise<Result<AddHabitResponseBody['habit'],string>> {
+    this.ensureToken();
+
+    try {
+      const response = await this.http.request<AddHabitResponseBody>({
+        method: 'POST',
+        url: '/habits',
+        headers: {
+          'Authorization': `Bearer ${this.token}`,
+        },
+        data: {
+          name: name,
+          category: category,
+          type: type,
+        },
+      });
+      return ok(response.data.habit);
+    }
+    catch (error) {
+      const errorMessage: string = this.handleError(error);
+      return err(errorMessage);
+    }
+  }
+
+  async getHabit(habitId: string): Promise<Result<GetHabitResponseBody['habit'],string>> {
+    this.ensureToken();
+
+    try {
+      const response = await this.http.request<GetHabitResponseBody>({
+        method: 'GET',
+        url: `/habits/${habitId}`,
+        headers: {
+          'Authorization': `Bearer ${this.token}`,
+        },
+      });
+      return ok(response.data.habit);
+    }
+    catch (error) {
+      const errorMessage: string = this.handleError(error);
+      return err(errorMessage);
+    }
+  }
+
+  async updateHabit(habitId: string, name?: string, category?: string): Promise<Result<null,string>> {
+    this.ensureToken();
+
+    try {
+      const response = await this.http.request<SuccessResponseBody>({
+        method: 'PUT',
+        url: `/habits/${habitId}`,
+        headers: {
+          'Authorization': `Bearer ${this.token}`,
+        },
+        data: {
+          name: name,
+          category: category,
+        },
+      });
+      return ok(null);
+    }
+    catch (error) {
+      const errorMessage: string = this.handleError(error);
+      return err(errorMessage);
+    }
+  }
+
+  private async archiveHabitHelper(habitId: string, archive: boolean): Promise<Result<null,string>> {
+    this.ensureToken();
+
+    try {
+      const response = await this.http.request<SuccessResponseBody>({
+        method: 'PUT',
+        url: `/habits/${habitId}`,
+        headers: {
+          'Authorization': `Bearer ${this.token}`,
+        },
+        data: {
+          archived: archive,
+        },
+      });
+      return ok(null);
+    }
+    catch (error) {
+      const errorMessage: string = this.handleError(error);
+      return err(errorMessage);
+    }
+  }
+
+  async archiveHabit(habitId: string): Promise<Result<null,string>> {
+    return await this.archiveHabitHelper(habitId, true);
+  }
+
+  async unarchiveHabit(habitId: string): Promise<Result<null,string>> {
+    return await this.archiveHabitHelper(habitId, false);
+  }
+
+  async deleteHabit(habitId: string): Promise<Result<null,string>> {
+    this.ensureToken();
+
+    try {
+      const response = await this.http.request<SuccessResponseBody>({
+        method: 'DELETE',
+        url: `/habits/${habitId}`,
+        headers: {
+          'Authorization': `Bearer ${this.token}`,
+        },
+      });
+      return ok(null);
+    }
+    catch (error) {
+      const errorMessage: string = this.handleError(error);
+      return err(errorMessage);
+    }
+  }
+
+  async getHabitHistory(habitId: string): Promise<Result<GetHabitHistoryResponseBody['history'],string>> {
+    this.ensureToken();
+
+    try {
+      const response = await this.http.request<GetHabitHistoryResponseBody>({
+        method: 'GET',
+        url: `/habits/${habitId}/history`,
+        headers: {
+          'Authorization': `Bearer ${this.token}`,
+        },
+      });
+      return ok(response.data.history);
+    }
+    catch (error) {
+      const errorMessage: string = this.handleError(error);
+      return err(errorMessage);
+    }
+  }
+
+  private async updateHabitHistoryHelper(habitId: string, date: DateTime, type: HistoryEntryType): Promise<Result<null,string>> {
+    this.ensureToken();
+
+    try {
+      const response = await this.http.request<SuccessResponseBody>({
+        method: 'POST',
+        url: `/habits/${habitId}/history`,
+        headers: {
+          'Authorization': `Bearer ${this.token}`,
+        },
+        data: {
+          date: date.toUTC().toISO(),
+          type: type,
+        },
+      });
+      return ok(null);
+    }
+    catch (error) {
+      const errorMessage: string = this.handleError(error);
+      return err(errorMessage);
+    }
+  }
+
+  async completeHabit(habitId: string, date: DateTime): Promise<Result<null,string>> {
+    return await this.updateHabitHistoryHelper(habitId, date, HistoryEntryType.COMPLETED);
+  }
+
+  async skipHabit(habitId: string, date: DateTime) {
+    return await this.updateHabitHistoryHelper(habitId, date, HistoryEntryType.SKIPPED);
+  }
+
+  async deleteHabitHistoryEntry(habitId: string, date: DateTime): Promise<Result<null,string>> {
+    this.ensureToken();
+
+    const dateStringParam = date.toISODate();
+
+    try {
+      const response = await this.http.request<SuccessResponseBody>({
+        method: 'DELETE',
+        url: `/habits/${habitId}/history/${dateStringParam}`,
+        headers: {
+          'Authorization': `Bearer ${this.token}`,
+        },
+      });
+      return ok(null);
+    }
+    catch (error) {
+      const errorMessage: string = this.handleError(error);
+      return err(errorMessage);
+    }
+  }
+
+
+
+  // Statistics
+
+  async getGeneralStats(): Promise<Result<GetGeneralStatsResponseBody['stats'],string>> {
+    this.ensureToken();
+
+    try {
+      const response = await this.http.request<GetGeneralStatsResponseBody>({
+        method: 'GET',
+        url: `/stats`,
+        headers: {
+          'Authorization': `Bearer ${this.token}`,
+        },
+      });
+      return ok(response.data.stats);
+    }
+    catch (error) {
+      const errorMessage: string = this.handleError(error);
+      return err(errorMessage);
+    }
+  }
+
+  async getHabitStats(habitId: string): Promise<Result<GetHabitStatsResponseBody['stats'],string>> {
+    this.ensureToken();
+
+    try {
+      const response = await this.http.request<GetHabitStatsResponseBody>({
+        method: 'GET',
+        url: `/stats/${habitId}`,
+        headers: {
+          'Authorization': `Bearer ${this.token}`,
+        },
+      });
+      return ok(response.data.stats);
+    }
+    catch (error) {
+      const errorMessage: string = this.handleError(error);
+      return err(errorMessage);
+    }
+  }
+
+
+
+  // Categories
+
+  async getCategories(): Promise<Result<GetCategoriesResponseBody['categories'],string>> {
+    this.ensureToken();
+
+    try {
+      const response = await this.http.request<GetCategoriesResponseBody>({
+        method: 'GET',
+        url: `/categories`,
+        headers: {
+          'Authorization': `Bearer ${this.token}`,
+        },
+      });
+      return ok(response.data.categories);
+    }
+    catch (error) {
+      const errorMessage: string = this.handleError(error);
+      return err(errorMessage);
+    }
+  }
+
+  async updateCategoryName(oldName: string, newName: string): Promise<Result<GetCategoriesResponseBody['categories'],string>> {
+    this.ensureToken();
+
+    try {
+      const response = await this.http.request<GetCategoriesResponseBody>({
+        method: 'PUT',
+        url: `/categories/${oldName}`,
+        headers: {
+          'Authorization': `Bearer ${this.token}`,
+        },
+        data: {
+          name: newName,
+        },
+      });
+      return ok(response.data.categories);
+    }
+    catch (error) {
+      const errorMessage: string = this.handleError(error);
+      return err(errorMessage);
+    }
+  }
+
+
+
+  // Error handling
+
+
+  private handleAxiosError(error: AxiosError): string {
+    let errorMessage: string;
+
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.warn(error.response.data);
+      console.warn(error.response.status);
+      console.warn(error.response.headers);
+      const errorBody = error.response.data as ErrorResponseBody;
+      errorMessage = errorBody.errorMessage
+    }
+    else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.warn(error.request);
+      errorMessage = 'Timeout error';
+      // TODO valutare un throw (in generale per gli errori di timeout)
+    }
+    else {
+      // Something happened in setting up the request that triggered an Error
+      console.error('Error', error.message);
+      errorMessage = 'Unknown error';
+    }
+    console.warn(error.config);
+    return errorMessage;
+  }
+
+  private handleError(error: any): string {
+    let errorMessage: string;
+    if (axios.isAxiosError(error)) {
+      // It's an AxiosError
+      errorMessage = this.handleAxiosError(error);
+    }
+    else {
+      // It's another type of error
+      errorMessage = 'Unknown error';
+      console.error(error);
+    }
+    return errorMessage;
+  }
+
 }
 
 
 /////////////////////////////////////////////////////////
 
-// TODO maybe una classe che contenga token e i seguenti metodi
 type Ok<T,E> = { success: true, value: T };
 type Err<T,E> = { success: false, error: E };
 type Result<T,E> = Ok<T,E> | Err<T,E>;
@@ -127,101 +519,11 @@ type Result<T,E> = Ok<T,E> | Err<T,E>;
 const ok = <T, E>(value: T): Result<T, E> => ({ success: true, value: value });
 const err = <T, E>(error: E): Result<T, E> => ({ success: false, error: error });
 
-// Authentication
 
-function makeHttpRequest() {
 
-}
-
-async function register(name: string, email: string, password: string): Promise<Result<string,string>> {
-  return ok('ciao');
-}
-
-async function login(email: string, password: string) {
-
-}
 
 
 async function logout() {
-
+  // TODO capire se va inserita o no
 }
 
-
-// Account management
-
-async function getAccountInfo() {
-
-}
-
-async function deleteAccount() {
-
-}
-
-
-// Habit
-
-async function getHabits(skip?: number, limit?: number, category?: string, filter?: 'all' | 'archived' | 'active') {
-
-}
-
-async function createHabit(name: string, category: string, type: 'DAILY' | 'WEEKLY' | 'MONTHLY') {
-  // TODO creare enum come lato server
-}
-
-async function getHabit(habitId: string) {
-
-}
-
-async function updateHabit(habitId: string, name?: string, category?: string) {
-
-}
-
-async function archiveHabit(habitId: string) {
-
-}
-
-async function unarchiveHabit(habitId: string) {
-
-}
-
-async function deleteHabit(habitId: string) {
-
-}
-
-async function getHabitHistory(habitId: string) {
-
-}
-
-async function completeHabit(habitId: string, date: DateTime) {
-
-}
-
-async function skipHabit(habitId: string, date: DateTime) {
-
-}
-
-async function deleteHabitHistoryEntry(habitId: string, date: DateTime) {
-
-}
-
-
-// Statistics
-
-async function getGeneralStats() {
-
-}
-
-async function getHabitStats(habitId: string) {
-
-}
-
-
-// Categories
-
-async function getCategories() {
-
-}
-
-async function updateCategoryName(oldName: string, newName: string) {
-
-}
